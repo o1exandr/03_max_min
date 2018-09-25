6dfbabe2d0970f0cc43bdb11178039cf8ae2802d
olexandr.finyuk <olexandr.finyuk@gmail.com>
2018-09-25 18:21:17 +0300

    done

--- 03_max_min/Program.cs
+++ 03_max_min/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ Завдання 3. 
+ Визначити метод для знаходження у одновимірному масиву максимального та мінімального. 
+    Метод повинен повертати ці значення через свої параметри. 
+    void MaxMin(int [] arr, .... int max, .... int min)
+ */
+
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03_max_min
 {
     class Program
     {
+        static void MaxMin(int[] arr, out int max, out int min)
+        {
+            max = arr.Max();
+            min = arr.Min();
+        }
+
         static void Main(string[] args)
         {
+            int[] arr = { -3, 5, 9, 1, 17, -5, 0 };
+
+            foreach (var a in arr)
+                Console.Write($"{a}\t");
+
+            MaxMin(arr, out int max, out int min);
+            Console.WriteLine($"\nMax = {max}\nMin = {min}");
         }
     }
 }
